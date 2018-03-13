871038eaeda2aa972f8b89f8b472f857002d7d9e
John Allen <john.allen@gmail.com>
2018-03-12 21:20:57 -0400

    update

--- http.js
+++ http.js
@@ -4,6 +4,13 @@ var express = require('express')
 var app = express()
 app.use(express.static(__dirname))
 
+// Example of a custom url
+// Sends OK to browser on /status url
+app.get('/status', (req, res) => {
+	res.send("OK")
+})
+
+
 // Listening on port 3000
 app.listen(3000, () => {
     console.log('server is listening mutha fucka')
